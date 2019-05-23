b9a3687f83041584152f942188e3611e3317262e
rodrigo <carlosrodrigoca31@gmail.com>
2019-05-23 16:34:01 -0400

    Navigate and create new pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default () => {
   return (
     <div>
       <p>Hello world!! this is our next js app!!!!!!</p>
+      <div>
+        <Link href="/list-cats">
+          <button>Check my list of cats</button>
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ pages/list-cats.js
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <div>
+      <h3>List of Cats!!!</h3>
+      <div>Cat 1</div>
+      <div>Cat 2</div>
+      <div>Cat 3</div>
+    </div>
+  );
+};
